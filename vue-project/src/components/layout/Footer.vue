e8f6e79f14cd110efacb3664e06a71618b3e1419
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2025-02-21 11:23:59 -0300

    initialize Vue 3 project with Vite, TypeScript, and Vuetify setup

---
+++ vue-project/src/components/layout/Footer.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-footer app color="primary" dark>
+    <v-col class="text-center" cols="12"> Meu Footer </v-col>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "AppFooter",
+};
+</script>
+
+<style scoped></style>
---
+++ vue-project/src/components/layout/Header.vue
@@ -0,0 +1,13 @@
+<template>
+  <v-app-bar app color="primary" dark>
+    <v-toolbar-title>TodoList</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+};
+</script>
+
+<style scoped></style>
