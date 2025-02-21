de5d3e78ff0a248881386f0bf38ad60034ade25d
Emanuel Sergio Girardi <emanuelsergiogirardi@gmail.com>
2025-02-21 16:08:04 -0300

    add main application layout with header, footer, and sidebar components

---
+++ vue-project/src/components/layout/Sidebar.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-navigation-drawer app>
+    <v-list>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-home</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Home</v-list-item-title>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-account</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Profile</v-list-item-title>
+      </v-list-item>
+      <v-list-item>
+        <v-list-item-icon>
+          <v-icon>mdi-settings</v-icon>
+        </v-list-item-icon>
+        <v-list-item-title>Settings</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  name: "AppSidebar",
+};
+</script>
